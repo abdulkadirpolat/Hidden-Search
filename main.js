530ab22ff81ea76f18e1955e46cbc0a69f11240d
abdulkadir <75859682+abdulkadirpolat@users.noreply.github.com>
2021-02-17 11:14:24 +0300

    Initial commit

--- main.js
+++ main.js
@@ -7,5 +7,19 @@ btn.addEventListener('click', () => {
 	searchBody.classList.toggle('active');
 	body.classList.toggle('color');
 	search.focus();
-    search.value = ''
+	showAlert(search.value);
 });
+
+function showAlert(val) {
+	if (search.value !== '') {
+		const div = document.createElement('div');
+		div.classList = 'message';
+		div.textContent = val;
+		alert.appendChild(div);
+
+		setTimeout(function () {
+			div.remove();
+		}, 1000);
+		search.value = '';
+	}
+}
